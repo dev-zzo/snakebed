e2eb2d78f768737b625c987c5b0d6e0ed511dafd
dev-zzo <gauri@tut.by>
2015-03-02 17:51:09 +0100

    interp: marked opcodes that are implemented

--- src/core/opcode.h
+++ src/core/opcode.h
@@ -9,30 +9,30 @@ extern "C" {
 typedef enum _SbOpcode {
     /* ??? */
     StopCode                = 0,
-    PopTop                  = 1,
-    RotTwo                  = 2,
-    RotThree                = 3,
-    DupTop                  = 4,
-    RotFour                 = 5,
-    Nop                     = 9,
+/**/PopTop                  = 1,
+/**/RotTwo                  = 2,
+/**/RotThree                = 3,
+/**/DupTop                  = 4,
+/**/RotFour                 = 5,
+/**/Nop                     = 9,
 
-    UnaryPositive           = 10,
-    UnaryNegative           = 11,
+/**/UnaryPositive           = 10,
+/**/UnaryNegative           = 11,
     UnaryNot                = 12,
     UnaryConvert            = 13,
-    UnaryInvert             = 15,
+/**/UnaryInvert             = 15,
 
     BinaryPower             = 19,
-    BinaryMultiply          = 20,
-    BinaryDivide            = 21,
-    BinaryModulo            = 22,
-    BinaryAdd               = 23,
-    BinarySubtract          = 24,
+/**/BinaryMultiply          = 20,
+/**/BinaryDivide            = 21,
+/**/BinaryModulo            = 22,
+/**/BinaryAdd               = 23,
+/**/BinarySubtract          = 24,
     BinarySubscript         = 25,
-    BinaryFloorDivide       = 26,
-    BinaryTrueDivide        = 27,
-    InPlaceFloorDivide      = 28,
-    InPlaceTrueDivide       = 29,
+/**/BinaryFloorDivide       = 26,
+/**/BinaryTrueDivide        = 27,
+/**/InPlaceFloorDivide      = 28,
+/**/InPlaceTrueDivide       = 29,
 
     Slice                   = 30,
     /* Also uses 31-33 */
@@ -44,37 +44,38 @@ typedef enum _SbOpcode {
     /* Also uses 51-53 */
 
     StoreMap                = 54,
-    InPlaceAdd              = 55,
-    InPlaceSubtract         = 56,
-    InPlaceMultiply         = 57,
-    InPlaceDivide           = 58,
-    InPlaceModulo           = 59,
+/**/InPlaceAdd              = 55,
+/**/InPlaceSubtract         = 56,
+/**/InPlaceMultiply         = 57,
+/**/InPlaceDivide           = 58,
+/**/InPlaceModulo           = 59,
     StoreSubscript          = 60,
     DeleteSubscript         = 61,
 
-    BinaryLeftShift         = 62,
-    BinaryRightShift        = 63,
-    BinaryAnd               = 64,
-    BinaryXor               = 65,
-    BinaryOr                = 66,
+/**/BinaryLeftShift         = 62,
+/**/BinaryRightShift        = 63,
+/**/BinaryAnd               = 64,
+/**/BinaryXor               = 65,
+/**/BinaryOr                = 66,
     InPlacePower            = 67,
     GetIter                 = 68,
 
+    /* Support not planned */
     PrintExpr               = 70,
     PrintItem               = 71,
     PrintNewline            = 72,
     PrintItemTo             = 73,
     PrintNewlineTo          = 74,
 
-    InPlaceLeftShift        = 75,
-    InPlaceRightShift       = 76,
-    InPlaceAnd              = 77,
-    InPlaceXor              = 78,
-    InPlaceOr               = 79,
+/**/InPlaceLeftShift        = 75,
+/**/InPlaceRightShift       = 76,
+/**/InPlaceAnd              = 77,
+/**/InPlaceXor              = 78,
+/**/InPlaceOr               = 79,
 
     BreakLoop               = 80,
     WithCleanup             = 81,
-    LoadLocals              = 82,
+/**/LoadLocals              = 82,
     ReturnValue             = 83,
     ImportStar              = 84,
     ExecStatement           = 85,
@@ -86,42 +87,42 @@ typedef enum _SbOpcode {
     HaveArgument            = 90,
     /* Opcodes from here have an argument: */
 
-    StoreName               = 90, /* Index in name list */
-    DeleteName              = 91, /* Index in name list */
+/**/StoreName               = 90, /* Index in name list */
+/**/DeleteName              = 91, /* Index in name list */
     UnpackSequence          = 92, /* Number of sequence items */
     ForIter                 = 93,
     ListAppend              = 94,
-    StoreAttr               = 95, /* Index in name list */
-    DeleteAttr              = 96, /* Index in name list */
-    StoreGlobal             = 97, /* Index in name list */
-    DeleteGlobal            = 98, /* Index in name list */
+/**/StoreAttr               = 95, /* Index in name list */
+/**/DeleteAttr              = 96, /* Index in name list */
+/**/StoreGlobal             = 97, /* Index in name list */
+/**/DeleteGlobal            = 98, /* Index in name list */
     DupTopX                 = 99, /* Number of items to duplicate */
-    LoadConst               = 100, /* Index in const list */
-    LoadName                = 101, /* Index in name list */
-    BuildTuple              = 102, /* Number of tuple items */
-    BuildList               = 103, /* Number of list items */
+/**/LoadConst               = 100, /* Index in const list */
+/**/LoadName                = 101, /* Index in name list */
+/**/BuildTuple              = 102, /* Number of tuple items */
+/**/BuildList               = 103, /* Number of list items */
     BuildSet                = 104, /* Number of set items */
     BuildMap                = 105, /* Always zero for now */
-    LoadAttr                = 106, /* Index in name list */
-    CompareOp               = 107, /* Comparison operator */
+/**/LoadAttr                = 106, /* Index in name list */
+/**/CompareOp               = 107, /* Comparison operator */
     ImportName              = 108, /* Index in name list */
     ImportFrom              = 109, /* Index in name list */
-    JumpForward             = 110, /* Number of bytes to skip */
-    JumpIfFalseOrPop        = 111, /* Target byte offset from beginning of code */
-    JumpIfTrueOrPop         = 112,
-    JumpAbsolute            = 113,
-    PopJumpIfFalse          = 114,
-    PopJumpIfTrue           = 115,
-    LoadGlobal              = 116, /* Index in name list */
+/**/JumpForward             = 110, /* Number of bytes to skip */
+/**/JumpIfFalseOrPop        = 111, /* Target byte offset from beginning of code */
+/**/JumpIfTrueOrPop         = 112,
+/**/JumpAbsolute            = 113,
+/**/PopJumpIfFalse          = 114,
+/**/PopJumpIfTrue           = 115,
+/**/LoadGlobal              = 116, /* Index in name list */
     ContinueLoop            = 119, /* Start of loop (absolute) */
     SetupLoop               = 120, /* Target address (relative) */
     SetupExcept             = 121,
     SetupFinally            = 122,
-    LoadFast                = 124, /* Local variable number */
-    StoreFast               = 125,
-    DeleteFast              = 126,
+/**/LoadFast                = 124, /* Local variable number */
+/**/StoreFast               = 125,
+/**/DeleteFast              = 126,
     RaiseVarArgs            = 130, /* Number of raise arguments (1, 2 or 3) */
-    CallFunction            = 131, /* #args + (#kwargs<<8) */
+/**/CallFunction            = 131, /* #args + (#kwargs<<8) */
     MakeFunction            = 132, /* #defaults */
     BuildSlice              = 133, /* Number of items */
 
