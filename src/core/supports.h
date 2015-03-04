7d142451a0f41405feef176527e65e8b0f4ce93a
dev-zzo <gauri@tut.by>
2015-03-04 15:14:36 +0100

    core: update supports defs

--- src/core/supports.h
+++ src/core/supports.h
@@ -3,6 +3,9 @@
 
 /* Define these to either 1 or 0. */
 
+/* Build with type checks in internal methods */
+#define SUPPORTS_BUILTIN_TYPECHECKS 1
+
 /* Interpreter supports */
 #define SUPPORTS_KWARGS 1
 #define SUPPORTS_WITH_STMT 0
