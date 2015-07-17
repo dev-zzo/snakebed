5bd091f6e55f38f7bbb3c8390848de353bd5c6ca
dev_zzo <gauri@tut.by>
2015-07-17 11:23:42 +0200

    socket: further tests
    ref: issue #3

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -11,14 +11,35 @@ class ProtocolIndependentTests(unittest.TestCase):
         socket.AF_INET
         socket.SOCK_STREAM
         socket.SOCK_DGRAM
-        
+
     def test_attributes(self):
         "Verify the socket object exposes expected attributes correctly"
+        sock = socket.socket()
+        try:
+            self.assertEqual(sock.family, socket.AF_INET)
+            self.assertEqual(sock.type, socket.SOCK_STREAM)
+            self.assertEqual(sock.proto, 0)
+        finally:
+            sock.close()
+
+    def test_settimeout(self):
+        "Verify timeout can be set and read back"
+        sock = socket.socket()
+        try:
+            sock.settimeout(5)
+            self.assertEqual(sock.gettimeout(), 5)
+        finally:
+            sock.close()
+
+    def test_connect_timeout(self):
+        "Verify the connect() method times out as set"
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.assertEqual(sock.family, socket.AF_INET)
-        self.assertEqual(sock.type, socket.SOCK_STREAM)
-        self.assertEqual(sock.proto, 0)
-        sock.close()
+        try:
+            sock.settimeout(1)
+            # NOTE: Currently, no HTTP service is on Google's DNS service...
+            self.assertRaises(socket.SocketTimeoutError, sock.connect, ("8.8.8.8", 80))
+        finally:
+            sock.close()
 #
 
 if __name__ == "__main__":
